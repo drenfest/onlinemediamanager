d314f349dde39515cb4f94bcf8a9095bddb636d5
drenfest <drenfest@gmail.com>
2021-02-22 13:12:34 -0600

    Added Users Functions

--- users/users.go
+++ users/users.go
@@ -1,5 +1,9 @@
 package users
 
+import (
+	"fmt"
+)
+
 type user struct {
 	Userid        int    `db:"user_id"`
 	Usertype      string `db:"user_type"`
@@ -13,4 +17,37 @@ type user struct {
 	city          string `db:"city"`
 	state         string `db:"state"`
 	zip           int    `db:"zip"`
+}
+type users []user
+
+// GET ALL USERS
+func (u users) getUsers() (users users, err error) {
+	if err != nil {
+		return users, fmt.Errorf("There was an error loading all users: %w ", err)
+	}
+	return users, nil
+}
+
+// GET USER BY ID
+func (u user) getUser(uid int) (user user, err error) {
+	if err != nil {
+		return user, fmt.Errorf("There was an error loading the user: %w ", err)
+	}
+	return user, nil
+}
+
+// UPDATE USER BY ID
+func (u user) updateUser() (user user, err error) {
+	if err != nil {
+		return user, fmt.Errorf("There was an error updating the user: %w ", err)
+	}
+	return user, nil
+}
+
+// DELETE USER BY ID
+func (u user) delUser() (user user, err error) {
+	if err != nil {
+		return user, fmt.Errorf("There was an error deleting the user: %w ", err)
+	}
+	return user, nil
 }
