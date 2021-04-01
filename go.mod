3d8e880c020c95f8d6ccabfb460290329bfb0790
The Web Guy <drenfest@gmail.com>
2021-04-01 08:48:31 -0500

    Added a repository to study how others are combining things in go

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module onlinemediamanager
 
 go 1.16
 
-require github.com/jmoiron/sqlx v1.3.1 // indirect
+require (
+	github.com/aerogo/aero v1.3.54
+	github.com/jmoiron/sqlx v1.3.1
+	github.com/zeebo/xxh3 v0.10.0 // indirect
+	golang.org/x/sys v0.0.0-20210225134936-a50acf3fe073 // indirect
+)
--- users/users.go
+++ users/users.go
@@ -1,9 +1,5 @@
 package users
 
-import (
-	"fmt"
-)
-
 type user struct {
 	Userid        int    `db:"user_id"`
 	Usertype      string `db:"user_type"`
@@ -14,40 +10,9 @@ type user struct {
 	Lastname      string `db:"last_name"`
 	Phone         int    `db:"phone"`
 	Streetaddress string `db:"street_address"`
-	city          string `db:"city"`
-	state         string `db:"state"`
-	zip           int    `db:"zip"`
+	City          string `db:"city"`
+	State         string `db:"state"`
+	Zip           int    `db:"zip"`
 }
+
 type users []user
-
-// GET ALL USERS
-func (u users) getUsers() (users users, err error) {
-	if err != nil {
-		return users, fmt.Errorf("There was an error loading all users: %w ", err)
-	}
-	return users, nil
-}
-
-// GET USER BY ID
-func (u user) getUser(uid int) (user user, err error) {
-	if err != nil {
-		return user, fmt.Errorf("There was an error loading the user: %w ", err)
-	}
-	return user, nil
-}
-
-// UPDATE USER BY ID
-func (u user) updateUser() (user user, err error) {
-	if err != nil {
-		return user, fmt.Errorf("There was an error updating the user: %w ", err)
-	}
-	return user, nil
-}
-
-// DELETE USER BY ID
-func (u user) delUser() (user user, err error) {
-	if err != nil {
-		return user, fmt.Errorf("There was an error deleting the user: %w ", err)
-	}
-	return user, nil
-}
